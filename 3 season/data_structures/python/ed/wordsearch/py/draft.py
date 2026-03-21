dc17faac62757518a5bd2d24047768571fb74b21
warrley <warleeyfarias@gmail.com>
2026-03-21 20:40:49 -0300

    att all

---
+++ 3 season/data_structures/python/ed/wordsearch/py/draft.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    strings: list[str] = [input() for _ in range(4)]
+    print("")
+    print(strings) 
+
+if __name__ == "__main__": main()
---
+++ 3 season/mobile_development/code/tests/init.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var monkey = "🐒"
+    val lion = "🦁"
+    // lion = "🐎" -> val cannot be changed
+    
+    println("$lion")
+}
