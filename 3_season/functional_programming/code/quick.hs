def749b0aa952b6cbe2de8918c21bc5ddbae83fa
warrley <warleeyfarias@gmail.com>
2026-03-24 23:53:24 -0300

    second and third version

--- 3_season/functional_programming/code/quick.hs
+++ 3_season/functional_programming/code/quick.hs
@@ -1,13 +1,20 @@
-quicksort ls = 
-    if length ls < 2
-        then ls
-    else 
-        x ++ [m] ++ y
-        where
-            m = ls !! 0
-            rest = drop 1 ls
-            x = quicksort [a | a <- rest, a <= m]
-            y = quicksort [a | a <- rest, a > m]
+quicksort [] = []
+quicksort (x:rest) = left ++ [x] ++ right 
+  where 
+    left = [a | a <- rest, a <= x]
+    right = [a | a <- rest, a > x]
+
+--quicksort ls = 
+--    if length ls < 2
+--        then ls
+--    else 
+--        x ++ [m] ++ y
+--        where
+--            m = ls !! 0
+--            rest = drop 1 ls
+--            x = quicksort [a | a <- rest, a <= m]
+--            y = quicksort [a | a <- rest, a > m]
+
 -- quicksort ls = 
 --     if length ls < 2
 --         then ls
