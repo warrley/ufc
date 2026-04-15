86b38dd8a33d461a8fc321bcad3376a63d7d6c26
warrley <warleeyfarias@gmail.com>
2026-04-15 20:07:53 -0300

    att ufc

---
+++ 3_season/data_structures/c/vector.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  data int[];
+  size int;
+  capacity: int;
+} vector
+
+void vector_init(int capacity) *Vector {
+  vector v = v
+}
+
+int main() {
+  
+}
